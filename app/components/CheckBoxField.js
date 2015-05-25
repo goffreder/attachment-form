63978ae9a7d2dc364578d8432f9fbcdb86338c63
goffreder <goffreder@gmail.com>
2015-05-25 21:54:46 +0200

    Aggiunto componente CheckBoxField

---
+++ app/components/CheckBoxField.js
@@ -0,0 +1,21 @@
+export default class CheckBoxField extends React.Component {
+    render() {
+        return (
+            <div className="attachment-field">
+                <label htmlFor={this.props.name}>{this.props.label}</label>
+                <input
+                    id={this.props.name}
+                    type="checkbox"
+                    name={this.props.label}
+                    disabled={this.props.disabled}
+                />
+            </div>
+        );
+    }
+}
+
+CheckBoxField.propTypes = {
+    name: React.PropTypes.string.isRequired,
+    label: React.PropTypes.string.isRequired,
+    disabled: React.PropTypes.bool.isRequired
+};
